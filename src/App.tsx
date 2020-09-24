26a8400da948f0c66aad45cabf329c393ec5ed3b
Mushinako <ridoedee@gmail.com>
2020-09-24 16:57:30 -0700

    Start of actual page
    + Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,82 @@
 import React from 'react';
-import underConstruction from './under-construction.png';
-import './App.scss';
+import './scss/App.scss';
+import './scss/nav.scss';
 
-class Name extends React.Component {
+// class WhoIAm extends React.Component<{}, { pre: string, post: string, postChoices: string[], postId: number }> {
+//   timer?: NodeJS.Timeout;
+//   fillLength: number;
+
+//   readonly scriptEls: HTMLScriptElement[] = [];
+//   // readonly scriptUrls: string[] = [
+//   //   "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.0/gsap.min.js",
+//   //   "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.0/TextPlugin.min.js",
+//   //   "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.0/EasePack.min.js",
+//   // ];
+
+//   constructor(props: Readonly<{}>) {
+//     super(props);
+//     this.state = {
+//       pre: "I am ",
+//       post: "...",
+//       postChoices: [
+//         "a programmer",
+//         "a chemist",
+//         "a thinker",
+//         "an explorer",
+//         "a researcher"
+//       ],
+//       postId: 0
+//     };
+//     this.fillLength = Math.max(3, ...this.state.postChoices.map((val: string): number => val.length));
+//   }
+
+//   componentDidMount(): void {
+//     // for (const scriptUrl of this.scriptUrls) {
+//     //   const scriptEl: HTMLScriptElement = appendScript(scriptUrl);
+//     //   this.scriptEls.push(scriptEl);
+//     // }
+//     this.timer = setInterval((): void => this.nextPost(), 2000);
+//   }
+
+//   componentWillUnmount(): void {
+//     // removeScripts(this.scriptEls);
+//     if (this.timer !== undefined)
+//       clearInterval(this.timer);
+//   }
+
+//   fillChoice(s: string): string {
+//     return s + " ".repeat(this.fillLength - s.length);
+//   }
+
+//   nextPost() {
+//     let nextPostId: number = (this.state.postId + 1) % this.state.postChoices.length;
+//     let nextPost: string = this.state.postChoices[nextPostId];
+//     this.setState({ post: nextPost, postId: nextPostId });
+//     this.transition();
+//   }
+
+//   transition(): void { }
+
+//   render(): JSX.Element {
+//     return (
+//       <div id="who-i-am">
+//         <h1 className="i-am">{this.state.pre}</h1>
+//         <h1 className="i-am">{this.fillChoice(this.state.post)}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+class Header extends React.Component {
   render(): JSX.Element {
     return (
-      <div id="name">
-        <h1>Lemuel Li</h1>
-      </div>
-    );
-  }
-}
-
-class WhoIAm extends React.Component<{}, { pre: string, post: string, postChoices: string[], postId: number }> {
-  timer?: NodeJS.Timeout;
-  fillLength: number;
-
-  constructor(props: Record<string, string>) {
-    super(props);
-    this.state = {
-      pre: "I am ",
-      post: "...",
-      postChoices: [
-        "a programmer",
-        "a chemist",
-        "a thinker",
-        "an explorer",
-        "a researcher"
-      ],
-      postId: 0
-    };
-    this.fillLength = Math.max(3, ...this.state.postChoices.map((val: string): number => val.length));
-  }
-
-  componentDidMount(): void {
-    this.timer = setInterval((): void => this.nextPost(), 2000);
-  }
-
-  componentWillUnmount(): void {
-    if (this.timer !== undefined)
-      clearInterval(this.timer);
-  }
-
-  fillChoice(s: string): string {
-    return s + " ".repeat(this.fillLength - s.length);
-  }
-
-  nextPost() {
-    let nextPostId: number = (this.state.postId + 1) % this.state.postChoices.length;
-    let nextPost: string = this.state.postChoices[nextPostId];
-    this.setState({ post: nextPost, postId: nextPostId });
-    this.transition();
-  }
-
-  transition(): void { }
-
-  render(): JSX.Element {
-    return (
-      <div id="who-i-am">
-        <h1 className="i-am">{this.state.pre}</h1>
-        <h1 className="i-am">{this.fillChoice(this.state.post)}</h1>
-      </div>
-    )
-  }
-}
-
-class KeepTuned extends React.Component {
-  render(): JSX.Element {
-    return (
-      <div id="keep-tuned">
-        <p>Under construction! Keep tuned!</p>
-        <img src={underConstruction} alt="under construction" className="under-construction-pic" />
-      </div>
+      <nav className="nav-bar">
+        <div className="nav-elements">
+          <div className="nav-el"><a href="/#">Mushinako</a></div>
+          <div className="nav-el"><a href="https://github.com/Mushinako">GitHub</a></div>
+          <div className="nav-el"><a href="/#">Placeholder</a></div>
+        </div>
+      </nav>
     )
   }
 }
@@ -79,10 +84,10 @@ class KeepTuned extends React.Component {
 function App(): JSX.Element {
   return (
     <div className="App">
+      <header>
+        <Header />
+      </header>
       <main id="main">
-        <Name />
-        <WhoIAm />
-        <KeepTuned />
       </main>
     </div>
   );
